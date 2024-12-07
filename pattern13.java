9d0cc5ad77de76df9f35188ca77b691addfcddaf
aejas389 <23hr1a3252@mtieat.org>
2024-12-07 16:21:07 +0530

    Add files via upload

---
+++ pattern13.java
@@ -0,0 +1,23 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class pattern13 {
+   public pattern13() {
+   }
+
+   public static void main(String[] var0) {
+      System.out.println("break example");
+
+      int var1;
+      for(var1 = 0; var1 < 5 && var1 != 2; ++var1) {
+         System.out.println(var1);
+      }
+
+      System.out.println("Continue example");
+
+      for(var1 = 0; var1 <= 5; ++var1) {
+         if (var1 != 2) {
+            System.out.println(var1);
+         }
+      }
+
+   }
+}
